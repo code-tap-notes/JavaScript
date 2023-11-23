88a136af3cff34067a90619f7b77dc8c8f5794ab
full-tech <nguyenuongvien@gmai.com>
2023-11-23 22:06:03 +0100

    thu xem khac nhau giua var va let

--- video3/video3.js
+++ video3/video3.js
@@ -1,4 +1,5 @@
-let c = 3,
-    b = 3.1416;
-let x = "16" + "Volvo";
+let c = 3,   b = 3.1416;
+var x = "16" + "Volvo";
+var n = null;
+console.log(n * 32);
 console.log("this 's using variable:", x, c + b);
