1505463e5b680031ea1ba9e7d503dfdac9aa942a
code-tap-notes <nguyentuongvien@yahoo.com>
2023-11-24 11:56:51 +0100

    array and object

---
+++ video13/video13.js
@@ -0,0 +1,12 @@
+console.log('so sanh object va array');
+console.log('object ', typeof person, person);
+let f = ['cam', 'tao', 'nho'];
+console.log('type is ', typeof f, f);
+// ket qua khac nhau o prototype va array tu dong co do dai
+console.log('first is ', f[0]);
+let food = [
+    fruit = f,
+    drink = ['water', 'wine', 'orange juice'],
+    disk = { meat: 'beef', sauce: 'thai' }
+]
+console.log('my foods are ', food);
